2bdd06dd86d67490845e12136815f4988dba8921
Mikko De Los Reyes <mikkode212@gmail.com>
2022-03-02 15:33:52 -0500

    fetch pokemon data from api, and append image from separate api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
-import Head from 'next/head'
-import Layout from "../components/Layout" 
+import Head from "next/Head";
+import Layout from "../components/Layout";
 
-export default function Home() {
+export default function Home({ pokemon }) {
+  console.log(pokemon);
   return (
     <Layout title="NextJS Pokedex">
-        <h1>NextJS Pokedex</h1>
+      <h1 className="text-4xl mb-8 text-center">NextJS Pokedex</h1>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+    const { results } = await res.json();
+    const pokemon = results.map((result, index) => {
+      const paddedIndex = ("00" + (index + 1)).slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+      return {
+        ...result,
+        image,
+      };
+    });
+    return {
+      props: { pokemon },
+    };
+  } catch (err) {
+    console.log(err);
+  }
 }
