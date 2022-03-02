13a6d0df4d7d8523941fd05fb3698ac44cbfabdf
Mikko De Los Reyes <mikkode212@gmail.com>
2022-03-02 15:48:20 -0500

    added more tailwinf styling to pokemon list

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ pokemon }) {
         {pokemon.map((pokeman, index) => (
           <li key={index}>
             <Link href={`/pokemon?id=${index +1}`}>
-              <a className="border p-4 border-gray my-2 ">
+              <a className="border p-4 border-gray my-2 capitalize flex items-center text-lg bg-gray-200 rounded-md">
                 <img className="w-20 h-20 mr-3" src={pokeman.image} alt={pokeman.name}/>
                 <span>{index + 1}. </span>
                 {pokeman.name}
