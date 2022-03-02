7afc3071f3a70b2a328aec766c04bb4220d85ba1
Mikko De Los Reyes <mikkode212@gmail.com>
2022-03-02 15:06:17 -0500

    added tailwind components and initial container

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,10 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Layout from "../components/Layout" 
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        
-      </main>
-
-    
-    </div>
+    <Layout title="NextJS Pokedex">
+        <h1>NextJS Pokedex</h1>
+    </Layout>
   )
 }
